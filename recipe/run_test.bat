ac902ce0820eaebcc9eb70f1344b848b974617e5
Mark Harfouche <mark.harfouche@gmail.com>
2025-12-19 22:07:16 -0500

    Try again for windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -6,9 +6,6 @@ set "G_MESSAGES_DEBUG=all"
 
 for /f "usebackq tokens=*" %%a in (`pkg-config --cflags libsoup-3.0`) do set "PC_CFLAGS=%%a"
 for /f "usebackq tokens=*" %%a in (`pkg-config --msvc-syntax --libs libsoup-3.0`) do set "PC_LIBS=%%a"
-REM it seems like libm is accidentally included through icu
-REM https://github.com/conda-forge/icu-feedstock/issues/42
-set "PC_LIBS=%PC_LIBS: m.lib=%"
 
 ECHO %PC_CFLAGS%
 ECHO %PC_LIBS%
