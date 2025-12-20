5e477417d66e39e8e63f0ad17318e20beac15750
Mark Harfouche <mark.harfouche@gmail.com>
2025-12-20 07:47:49 -0500

    Try to fix for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,27 @@
 @ECHO ON
 
+REM hmaarrfk -- 2025/12/19
+REM Requires.private and Libs.private
+REM Are not meaningful in the context of shared libraries for conda-forge
+REM We thus "remove them" outright to avoid
+REM burdening the recipe
+REM https://github.com/conda-forge/harfbuzz-feedstock/pull/146
+REM https://github.com/conda-forge/conda-forge.github.io/issues/1880
+
+setlocal EnableExtensions EnableDelayedExpansion
+
+set "PCDIR=%PREFIX%\Library\lib\pkgconfig"
+
+REM CMD has no in-place edit, so rewrite each file via a temporary file
+for /R "%PCDIR%" %%F in (*.pc) do (
+    set "TMP=%%F.tmp"
+    REM Drop Requires.private and Libs.private lines
+    findstr /V /R "^Requires\.private ^Libs\.private" "%%F" > "!TMP!"
+    move /Y "!TMP!" "%%F" > nul
+)
+
+endlocal
+
 :: set pkg-config path so that host deps can be found
 :: (set as env var so it's used by both meson and during build with g-ir-scanner)
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
@@ -33,3 +55,23 @@ REM https://github.com/conda-forge/libsoup-feedstock/issues/46
 del %LIBRARY_LIB%\\girepository-1.0\\Soup*.typelib
 del %LIBRARY_PREFIX%\share\gir-1.0\Soup*.gir
 del %LIBRARY_PREFIX%\bin\*.pdb
+
+REM hmaarrfk -- 2025/12/19
+REM Requires.private and Libs.private
+REM Are not meaningful in the context of shared libraries for conda-forge
+REM We thus "remove them" outright to avoid
+REM burdening the recipe
+REM https://github.com/conda-forge/harfbuzz-feedstock/pull/146
+REM https://github.com/conda-forge/conda-forge.github.io/issues/1880
+
+setlocal EnableExtensions EnableDelayedExpansion
+
+REM CMD has no in-place edit, so rewrite each file via a temporary file
+for /R "%PCDIR%" %%F in (*.pc) do (
+    set "TMP=%%F.tmp"
+    REM Drop Requires.private and Libs.private lines
+    findstr /V /R "^Requires\.private ^Libs\.private" "%%F" > "!TMP!"
+    move /Y "!TMP!" "%%F" > nul
+)
+
+endlocal
--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,5 +1,27 @@
 @echo on
 
+REM hmaarrfk -- 2025/12/19
+REM Requires.private and Libs.private
+REM Are not meaningful in the context of shared libraries for conda-forge
+REM We thus "remove them" outright to avoid
+REM burdening the recipe
+REM https://github.com/conda-forge/harfbuzz-feedstock/pull/146
+REM https://github.com/conda-forge/conda-forge.github.io/issues/1880
+
+setlocal EnableExtensions EnableDelayedExpansion
+
+set "PCDIR=%PREFIX%\Library\lib\pkgconfig"
+
+REM CMD has no in-place edit, so rewrite each file via a temporary file
+for /R "%PCDIR%" %%F in (*.pc) do (
+    set "TMP=%%F.tmp"
+    REM Drop Requires.private and Libs.private lines
+    findstr /V /R "^Requires\.private ^Libs\.private" "%%F" > "!TMP!"
+    move /Y "!TMP!" "%%F" > nul
+)
+
+endlocal
+
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig"
 set "GIO_MODULE_DIR="
 set "G_MESSAGES_DEBUG=all"
